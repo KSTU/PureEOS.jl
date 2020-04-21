7c114693f9a43b38fed0b2f6d22a10db76542596
Ivan Anashkin <anashkin.ivan@gmail.com>
2020-04-21 16:45:58 +0300

    g fix

--- src/LJ2.jl
+++ src/LJ2.jl
@@ -194,18 +194,19 @@ sub.ro=ro
 sub.p=(1.0+phidelta*delta)*ro*T	#bar
 sub.z=1.0+phidelta*delta	#
 #ideal part
-n0=[9.212802589, 
--4.682248550,
-3.003039265,
-1.117433359,
-3.467773215,
-6.941944640,
-5.970850948,
+n0=[6.262265814, 
+-1.515151515,
+1.5,
+0.0,
+0.0,
+0.0,
+0.0,
 0.0]
 gamma0=[0.0, 0.0, 0.0, 1.4091052332, 4.0099170712, 6.5967098342, 13.9798102659,1.0]
 
 #entropy
 phi0=log(delta)+n0[1]+n0[2]*tau+n0[3]*log(tau)+n0[4]*log(1.0-exp(-gamma0[4]*tau))+n0[5]*log(1.0-exp(-gamma0[5]*tau))+n0[6]*log(1.0-exp(-gamma0[6]*tau))+n0[7]*log(1.0-exp(-gamma0[7]*tau))+n0[8]*log(1.0-exp(-gamma0[8]*tau))
+
 phi0tau=n0[2]+n0[3]/tau+n0[4]*gamma0[4]*(1.0/(1.0-exp(-gamma0[4]*tau))-1.0)+n0[5]*gamma0[5]*(1.0/(1.0-exp(-gamma0[5]*tau))-1.0)+n0[6]*gamma0[6]*(1.0/(1.0-exp(-gamma0[6]*tau))-1.0)+n0[7]*gamma0[7]*(1.0/(1.0-exp(-gamma0[7]*tau))-1.0)+n0[8]*gamma0[8]*(1.0/(1.0-exp(-gamma0[8]*tau))-1.0)
 phi0delta=1/delta
 phi0delta2=-1/delta^2
